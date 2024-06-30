07c43cca7cfbe15ea0fbac74bc3d066ce6c9767e
ugwutobychuks <49158334+ugwutobychuks@users.noreply.github.com>
2024-06-30 06:36:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var name = "Ugwu Patrick Tobechukwu";
+  var nameDisplay = document.getElementById('nameDisplay');
+  nameDisplay.textContent = name;
+});
+
+
+
